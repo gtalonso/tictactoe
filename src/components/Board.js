c79f621676f399febe49eab24fc5e3d59f253a0a
Guillermo Torres <gtorres.alonso@gmail.com>
2020-12-20 18:10:13 -0600

    adding components

---
+++ src/components/Board.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Square from './Square';
+
+const Board = () => (
+  <div>
+    <div>
+      <Square />
+      <Square />
+      <Square />
+    </div>
+    <div>
+      <Square />
+      <Square />
+      <Square />
+    </div>
+    <div>
+      <Square />
+      <Square />
+      <Square />
+    </div>
+  </div>
+);
+
+export default Board;
---
+++ src/components/Square.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Square = () => <button type="button">1</button>;
+
+export default Square;
